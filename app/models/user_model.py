ff49e3338deb59cd4f409cfe9f15f7f7b4feb36a
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-16 20:49:34 +0800

    Update user_model.py

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -8,9 +8,20 @@ from dataclasses import dataclass
 
 @dataclass
 class User:
-    id: int
-    username: str
-    password_hash: str
-    role: str
-    must_change_password: int
-    created_at: str
+    def __init__(
+        self,
+        id,
+        username,
+        password_hash,
+        role,
+        is_active,
+        must_change_password,
+        created_at,
+    ):
+        self.id = id
+        self.username = username
+        self.password_hash = password_hash
+        self.role = role
+        self.is_active = is_active
+        self.must_change_password = must_change_password
+        self.created_at = created_at
